75a9849ae83ef3697c122d9ad6ef2b45c55bb033
Chandima Senarathna <chandima.senarathna97@gmail.com>
2025-04-26 13:38:15 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version = '0.0.0',
+    author = 'Chandima Senarathna',
+    author_email = 'chandima.senarathna97@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
